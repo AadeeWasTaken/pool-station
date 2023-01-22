5b851d7ee926aab79a4469bc10b3959661f49fdf
Rudra Patel <84099324+imrudrapatel@users.noreply.github.com>
2023-01-22 10:23:29 +0530

    Add Pool brrrr

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,4 +4,27 @@
 	export let data: PageData;
 </script>
 
-<h1 class="text-3xl font-bold underline">{data.pools[0].location}</h1>
+<div>
+	<div class="grid grid-cols-4 gap-y-16">
+		{#each data.pools as pool}
+			<div>
+				<div class="w-56 rounded-3xl">
+					<img src={pool.image} alt="location" class="h-56 object-cover rounded-3xl" />
+				</div>
+				<div>
+					<div class="w-56 flex justify-between">
+						<h3 class="text-lg ">{pool.location}</h3>
+						<div class="flex items-center">
+							<i class="fa-solid fa-star " />
+							<h5>{pool.interested}</h5>
+						</div>
+					</div>
+					<div>
+						<p>Invested Amount: <span>₹{pool.invested}</span></p>
+						<p>Targeted Amount: <span>₹{pool.cost}</span></p>
+					</div>
+				</div>
+			</div>
+		{/each}
+	</div>
+</div>
