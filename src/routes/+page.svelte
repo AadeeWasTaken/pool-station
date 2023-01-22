f360b8513cf8d66af2bab4de7238900f38d6a24e
Aadeetya <aadeetyapatil@gmail.com>
2023-01-22 00:10:04 -0500

    links go brrrrrrrrrrrrrrrrrrrrrrrrr

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,24 +10,26 @@
 	</div>
 	<div class="grid grid-cols-4 gap-y-16">
 		{#each data.pools as pool}
-			<div>
-				<div class="w-56 rounded-3xl">
-					<img src={pool.image} alt="location" class="h-56 object-cover rounded-3xl" />
-				</div>
+			<a href={'/pool/' + pool.id}>
 				<div>
-					<div class="w-56 flex justify-between">
-						<h3 class="text-lg ">{pool.location}</h3>
-						<div class="flex items-center">
-							<i class="fa-solid fa-star " />
-							<h5>{pool.interested}</h5>
-						</div>
+					<div class="w-56 rounded-3xl">
+						<img src={pool.image} alt="location" class="h-56 object-cover rounded-3xl" />
 					</div>
 					<div>
-						<p>Invested Amount: <span>₹{pool.invested}</span></p>
-						<p>Targeted Amount: <span>₹{pool.cost}</span></p>
+						<div class="w-56 flex justify-between">
+							<h3 class="text-lg ">{pool.location}</h3>
+							<div class="flex items-center">
+								<i class="fa-solid fa-star " />
+								<h5>{pool.interested}</h5>
+							</div>
+						</div>
+						<div>
+							<p>Invested Amount: <span>₹{pool.invested}</span></p>
+							<p>Targeted Amount: <span>₹{pool.cost}</span></p>
+						</div>
 					</div>
 				</div>
-			</div>
+			</a>
 		{/each}
 	</div>
 </div>
